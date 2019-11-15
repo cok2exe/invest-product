3141149785b13b04e589429d4e8f1529e52e467b
cok2exe <cok2exe@gmail.com>
2019-11-15 14:57:22 +0900

    update :) 더보기 페이지네이션 적용

--- src/containers/ProductList/index.js
+++ src/containers/ProductList/index.js
@@ -1,20 +1,50 @@
-import React from "react";
-import productList from "../../productList.json";
+import React, { useState, useEffect } from "react";
+import productData from "../../productList.json";
 import ProductCard from "../../components/ProductCard";
 
 import "./index.scss";
 
 function ProductList() {
-  console.log(productList);
+  const pageSize = 5;
+  const numberOfPages = Math.ceil(productData.total / pageSize);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productList, setProductList] = useState([]);
+
+  // 더보기 페이지네이션 기능
+  const calcPagination = _list => {
+    let accList = _list;
+    const targetIndex =
+      pageSize * currentPage < productData.total
+        ? pageSize * currentPage
+        : productData.total;
+
+    for (let i = pageSize * (currentPage - 1); i < targetIndex; i++) {
+      accList = accList.concat(productData.list[i]);
+    }
+
+    return accList;
+  };
+
+  useEffect(() => {
+    // currentPage 가 바뀔때마다 실행
+    const calcList = calcPagination(productList);
+    setProductList(calcList);
+  }, [currentPage]);
+
+  const viewMore = page => {
+    setCurrentPage(page);
+  };
+
   return (
     <div className="ProductList">
       <div className="result">
         <div className="total-count">
-          총 <span>{productList.total}</span>건의 상품이 검색되었습니다.
+          총 <span>{productData.total}</span>건의 상품이 검색되었습니다.
         </div>
 
         <div className="list">
-          {productList.list.map(product => {
+          {productList.map(product => {
             const achievementRate = Math.floor(
               (product.investAmount / product.targetAmount) * 100
             );
@@ -35,7 +65,14 @@ function ProductList() {
           })}
         </div>
 
-        <button className="btn btn-more">더보기 (1/페이지수)</button>
+        {currentPage < numberOfPages && (
+          <button
+            className="btn btn-more"
+            onClick={() => viewMore(currentPage + 1)}
+          >
+            더보기 ({currentPage}/{numberOfPages})
+          </button>
+        )}
       </div>
     </div>
   );
